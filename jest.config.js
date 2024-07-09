d7cba48fb8d91d7fc814dc83645bc40e346c7858
codeTester7 <testerqualityengineer7@gmail.com>
2024-07-09 10:51:50 +0530

    html report modified

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,13 @@ module.exports = {
   reporters : ['default', 
     ['jest-html-reporters',
       {
-      publicPath : "reports"
+      publicPath : "reports",
+      includePassed : true,
+      openReport : true,
+      expand : true,
+      includeCustomData : true,
+      includeFailureMsg : true,
+      includeConsoleLog : true
       }
     ]
   ]
