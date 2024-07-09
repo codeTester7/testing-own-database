1925b9ab3d457ce197b256fe1817963dcc659ccd
codeTester7 <testerqualityengineer7@gmail.com>
2024-07-09 17:17:00 +0530

    skip files added

--- test/upload.test.ts
+++ test/upload.test.ts
@@ -1,14 +1,14 @@
 import uploadController from "../controllers/upload.controller"
 
 describe('UPLOAD FILES', () => {
-    it('POST /upload/single', async () => {
+    it.skip('POST /upload/single', async () => {
         const res = await uploadController.postSingleUpload('');
         expect(res.body.filename).toEqual('');
         expect(res.statusCode).toBe(201);
         expect(res.body.length).toEqual(1);
     });
 
-    it('POST /upload/multiple', async () => {
+    it.skip('POST /upload/multiple', async () => {
         const files = [];
         const res = await uploadController.postMultipleUpload(files);
         expect(res.body[0].filename).toEqual('');
